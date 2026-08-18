6b181857b295a931300555ecea8c089b6236cc6b
Wind0ws11Aero <jasonrex1999@icloud.com>
2026-08-18 21:35:32 +0800

    Interesting changes thaht didn't update build, cause a lot of bugs

---
+++ bad_query/BQSettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  BQSettingsView.swift
+//  Jade
+//
+//  Created by Jason on 2026/8/18.
+//
+
+import SwiftUI
+
+struct BQSettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BQSettingsView()
+}
